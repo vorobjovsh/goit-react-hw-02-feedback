9fee86e0c4399fba168f0ad5b8de93b44f635aac
vorobjovsh <vorobjov75@gmail.com>
2022-07-28 15:06:13 +0300

    create step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,24 +26,41 @@ class App extends Component {
     }));
   };
 
+  countTotalFeedback = () => {
+    const totalSum = this.state.good + this.state.neutral + this.state.bad;
+    return totalSum;
+  }
+
+  countPositiveFeedbackPercentage = (item) => {
+    let positive = 0;
+    if (item !== 0) {
+      positive = this.state.good/(item/100);
+    }
+    return Math.round(positive);
+  }
+
   render() {
     const { good } = this.state;
     const { neutral } = this.state;
     const { bad } = this.state;
+    const total  = this.countTotalFeedback();
+    const posit = this.countPositiveFeedbackPercentage(total);
 
     return (
       <>
         <h2>Please leave feedback</h2>
         <div>
-          <button onClick={this.addGood}>Good</button>
-          <button onClick={this.addNeutral}>Neutral</button>
-          <button onClick={this.addBad}>Bad</button>
+          <button className='feed' onClick={this.addGood}>Good</button>
+          <button className='feed' onClick={this.addNeutral}>Neutral</button>
+          <button className='feed feed--last' onClick={this.addBad}>Bad</button>
         </div>
 
         <h2>Statistics</h2>
         <p>Good: <span>{good}</span></p>
         <p>Neutral: <span>{neutral}</span></p>
         <p>Bad: <span>{bad}</span></p>
+        <p>Total: <span>{total}</span></p>
+        <p>Positive feedback: <span>{posit}%</span></p>
       </>
 
     );
